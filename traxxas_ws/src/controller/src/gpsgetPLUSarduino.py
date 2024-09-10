a9ca85fadec7176c43b9e238acb7c5839de260e4
Volkan Taştan <volkan.tastan@metu.edu.tr>
2024-09-10 02:03:48 -0700

    base gps file writing working

--- traxxas_ws/src/controller/src/gpsgetPLUSarduino.py
+++ traxxas_ws/src/controller/src/gpsgetPLUSarduino.py
@@ -3,6 +3,7 @@
 import rospy
 from tcp_parser.msg import *
 from controller.msg import *
+from controller.srv import *
 
 lat=0
 lon=0
@@ -21,7 +22,18 @@ def do_nothing(gps):
 
     lat=gps.latitude
     lot=gps.longitude
+
+
+    try:
+            coordinate_getter = rospy.ServiceProxy("gps_to_coord", GPSCoordinate)
+            gps_new =  coordinate_getter(lat, lot)
+
+    except rospy.ServiceException as serviceError0:
+            rospy.logwarn("Service call failed: %s", serviceError0)
+
+
     rospy.loginfo("Latitude: " + str(lat) + " / Longitude: " + str(lot))
+    
     arduinoMessage = ControllerOut()
     if angle < 1 :
         angle += 0.05
@@ -41,6 +53,9 @@ def do_nothing(gps):
 if __name__ == '__main__':
     rospy.init_node("controller")
     rospy.loginfo("Node initialized.")
+    rospy.loginfo("Waiting for GPS Converter Server.")
+    rospy.wait_for_service("gps_to_coord")
+
     gps_sub = rospy.Subscriber("gps_data", GPSData, do_nothing)
     control_pub = rospy.Publisher("controller_out", ControllerOut, queue_size=5)
     #Controllerout has steer and speed parameters which take values between -1 and 1
